645a5382ab211e38798c05eae0f868ad90f033d4
Aaron Biliyok <aaronbiliyok@Aarons-MacBook-Pro.local>
2022-07-19 15:38:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def wallet_hub_ml_project():
+    # random print statement
+    print("first line of code for wallet hub ML Engineer assessment") 
+
+
+if __name__ == '__main__':
+    wallet_hub_ml_project()
+
+
